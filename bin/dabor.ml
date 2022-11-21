3e044421246cdd21973aec24689390f8dc7883b8
Annie Song <afsong2018@gmail.com>
2022-11-21 05:19:25 -0500

    fix dune

--- bin/dabor.ml
+++ bin/dabor.ml
@@ -1,1 +1,7 @@
-let () = print_endline "Hello, World!"
+open Plt_dabor
+
+let _ =
+  let lexbuf = Lexing.from_channel stdin in
+  let program = Parser.program_rule Scanner.token lexbuf in
+  (*let sprogram = Semant.check program in*)
+  print_endline (Ast.string_of_program program)
