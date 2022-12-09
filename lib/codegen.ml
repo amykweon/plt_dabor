e8a4fe8d55bb0727e0c5f7fb6ba04265088733bb
khaelaharrod <113210421+khaelaharrod@users.noreply.github.com>
2022-12-08 22:01:28 -0500

    Update codegen.ml
    added some arit

--- lib/codegen.ml
+++ lib/codegen.ml
@@ -10,3 +10,32 @@ open Sast
 
 module StringMap = Map.Make(String)
 
+let rec check_expr = function
+    | Binop(e1, op, e2) as e ->
+        let (t1, e1') = check_expr e1
+        and (t2, e2') = check_expr e2 in
+        let err = "illegal binary operator " ^
+                  string_of_typ t1 ^ " " ^ string_of_op op ^ " " ^
+                  string_of_typ t2 ^ " in " ^ string_of_expr e
+        in
+    
+        if t1 = t2 then
+          let t = match op with
+            | A.Add     -> L.build_fadd
+            | A.Sub     -> L.build_fsub
+            | A.Mult    -> L.build_fmul
+            | A.Div     -> L.build_fdiv 
+            | A.Mod     -> L.build_fmod
+            | A.Equal   -> L.build_fcmp L.Fcmp.Oeq
+            | A.Neq     -> L.build_fcmp L.Fcmp.One
+            | A.Less    -> L.build_fcmp L.Fcmp.Olt
+            | A.Leq     -> L.build_fcmp L.Fcmp.Oleq
+            | A.Greater -> L.build_fcmp L.Fcmp.Ogt
+            | A.Geq     -> L.build_fcmp L.Fcmp.Ogeq
+
+    | SUnop(op, (t1, e1') ) ->
+          let e' = expr builder e in
+            (match op with
+            | A.Neg     -> L.build_fneq 
+            | A.Neg     -> L.build_neq
+            | A.Not     -> L.build_not
