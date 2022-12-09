63c1669e6c4cbebc3c191ca78f6828712fe52bc8
Hemanth141414 <36065740+Hemanth141414@users.noreply.github.com>
2022-12-09 14:07:32 -0500

    Update codegen.ml
    Added if and while translation.

--- lib/codegen.ml
+++ lib/codegen.ml
@@ -64,3 +64,38 @@ let rec check_expr = function
             | A.Neg     -> L.build_fneq 
             | A.Neg     -> L.build_neq
             | A.Not     -> L.build_not
+            
+let rec stmt builder = function
+	SBlock sl -> List.fold_left stmt builder sl
+      SIf (predicate, then_stmt, else_stmt) ->
+         let bool_val = expr builder predicate in
+	     let merge_bb = L.append_block context "merge" the_function in
+         let build_br_merge = L.build_br merge_bb in (* partial function *)
+
+     let then_bb = L.append_block context "then" the_function in
+     add_terminal (stmt (L.builder_at_end context then_bb) then_stmt)
+       build_br_merge;
+
+     let else_bb = L.append_block context "else" the_function in
+     add_terminal (stmt (L.builder_at_end context else_bb) else_stmt)
+       build_br_merge;
+
+	 ignore(L.build_cond_br bool_val then_bb else_bb builder);
+	 L.builder_at_end context merge_bb
+
+      | SWhile (predicate, body) ->
+	  let pred_bb = L.append_block context "while" the_function in
+	  ignore(L.build_br pred_bb builder);
+
+	  let body_bb = L.append_block context "while_body" the_function in
+	  add_terminal (stmt (L.builder_at_end context body_bb) body)
+	    (L.build_br pred_bb);
+
+	  let pred_builder = L.builder_at_end context pred_bb in
+	  let bool_val = expr pred_builder predicate in
+
+	  let merge_bb = L.append_block context "merge" the_function in
+	  ignore(L.build_cond_br bool_val body_bb merge_bb pred_builder);
+	  L.builder_at_end context merge_bb
+      
+    in
