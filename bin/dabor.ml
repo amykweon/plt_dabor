ef967afff717b89884b16661299d5c3ac33a7f1b
Seoyoung Kweon <kweonamy@gmail.com>
2022-12-09 20:23:17 -0500

    trying...

--- bin/dabor.ml
+++ bin/dabor.ml
@@ -1,7 +1,28 @@
-open Plt_dabor
+type action = Ast | Sast | LLVM_IR | Compile
 
-let _ =
-  let lexbuf = Lexing.from_channel stdin in
-  let program = Parser.program_rule Scanner.token lexbuf in
-  let sprogram = Semant.check program in
-  print_endline (Sast.string_of_sprogram sprogram)
+let () =
+  let action = ref Compile in
+  let set_action a () = action := a in
+  let speclist = [
+    ("-a", Arg.Unit (set_action Ast), "Print the AST");
+    ("-s", Arg.Unit (set_action Sast), "Print the SAST");
+    ("-l", Arg.Unit (set_action LLVM_IR), "Print the generated LLVM IR");
+    ("-c", Arg.Unit (set_action Compile),
+      "Check and print the generated LLVM IR (default)");
+  ] in  
+  let usage_msg = "usage: ./dabor [-a|-s|-l|-c] [file.db]" in
+  let channel = ref stdin in
+  Arg.parse speclist (fun filename -> channel := open_in filename) usage_msg;
+  
+  let lexbuf = Lexing.from_channel !channel in
+  let ast = Plt_dabor.Parser.program Plt_dabor.Scanner.token lexbuf in  
+  match !action with
+    Ast -> print_string (Plt_dabor.Ast.string_of_program ast)
+  | _ -> let sast = Plt_dabor.Semant.check ast in
+    match !action with
+      Ast     -> ()
+    | Sast    -> print_string (Plt_dabor.Sast.string_of_sprogram sast)
+    | LLVM_IR -> print_string (Llvm.string_of_llmodule (Plt_dabor.Codegen.translate sast))
+    | Compile -> let m = Plt_dabor.Codegen.translate sast in
+	Llvm_analysis.assert_valid_module m;
+	print_string (Llvm.string_of_llmodule m)
